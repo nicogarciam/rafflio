ec38b5b52e8583352ac38917150e426e3f7d6906
Nicolas Garcia <nicogarciam@gmail.com>
2025-07-29 18:53:07 -0300

    vite port

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,11 +8,7 @@ export default defineConfig({
     'process.env': {}
   },
   server: {
-    https: {
-      key: fs.readFileSync('./server/server.key'),
-      cert: fs.readFileSync('./server/server.cert'),
-    },
-    port: 3000,
+    port: 5173,
     host: true
   },
   build: {
